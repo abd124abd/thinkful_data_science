82e16b1d1bdb12245ec57991d6103d63f1e0622d
cryptolemming <aliayoub22@gmail.com>
2019-10-26 16:17:07 -0400

    complete: s6 assignment

--- bootcamp/unit_1/l_2/s_6/q.sql
+++ bootcamp/unit_1/l_2/s_6/q.sql
@@ -43,8 +43,22 @@ FROM
 GROUP BY 1
 ORDER BY count DESC;
 
--- 3. Cheapest San Fran time of year
+-- 3. Cheapest San Fran time of year - Jan
+SELECT
+  DATE_TRUNC('month', calender_date) as month,
+  AVG(CAST(CASE WHEN price='' THEN '0' ELSE REPLACE(REPLACE(price, '$', ''), ',', '') END AS float)) as avg_price,
+  COUNT(*)
+FROM
+  sfo_calendar s
+GROUP BY 1
+ORDER BY avg_price;
 
-
-
--- 3b. Busiest
+-- 3b. Busiest - several tied
+SELECT
+  DATE_TRUNC('month', calender_date) as month,
+  AVG(CAST(CASE WHEN price='' THEN '0' ELSE REPLACE(REPLACE(price, '$', ''), ',', '') END AS float)) as avg_price,
+  COUNT(*)
+FROM
+  sfo_calendar s
+GROUP BY 1
+ORDER BY count DESC;
